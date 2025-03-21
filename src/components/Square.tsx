637ff1921ef0250ab63c37a3cf30c5d3e1213a51
Natan <barralagart@gmail.com>
2025-03-21 11:55:11 -0300

    implemented react tutorial Tic-Tac-Toe

---
+++ src/components/Square.tsx
@@ -0,0 +1,9 @@
+import '../css/style.css';
+
+export default function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Game from './Game';
+import './css/style.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>
 );
 
